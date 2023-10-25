615080372f38922520427bb64692dd1b4e62b63d
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-25 16:30:13 +0200

    [INIT]: file php

---
+++ pagina-invio-dati.form-get.php
@@ -0,0 +1,19 @@
+<?php
+
+$testo = echo $GET ['testo'];
+$parola = echo $GET ['parola'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Dati ricevuti dal form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    </body>
+</html>
